fa7adf3d611f4137954a52181e3a96a67d134eef
jimmaaay <thisguy@jimmythompson.me>
2019-09-25 21:03:03 +0100

    Added button component

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
+import { css } from 'glamor';
 import Button from './Button';
 
 export default {
@@ -7,6 +8,22 @@ export default {
   component: Button,
 };
 
+const rules = css({
+  '> * ': {
+    margin: '0 1rem',
+  }
+});
 
 
-export const text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
+
+export const text = () => (
+  <div { ...rules } >
+    <Button onClick={action('clicked')}>Primary</Button>
+    <Button
+      theme="delete"
+      onClick={action('clicked')}
+    >
+      Delete
+    </Button>
+  </div>
+)
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,22 +6,57 @@ import { css } from 'glamor';
 // 545766
 
 interface ButtonProps {
-  onClick: MouseEventHandler<HTMLButtonElement>;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  theme?: 'primary' | 'delete';
 }
 
-const rule = css({
+const rules = css({
   fontSize: '1.6rem',
   fontWeight: 'normal',
-  fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol',
-
-  background: 
+  fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+  padding: '1rem 2rem',
+  border: 'none',
+  cursor: 'pointer',
+  borderRadius: '5px',
+  transition: 'background 0.2s ease-in-out',
 });
 
+const themes = {
+  primary: {
+    background: 'hsl(41, 98%, 54%)',
+    color: '#fff',
+    ':hover': {
+      background: 'hsl(41, 98%, 46%)'
+    },
+  },
+  delete: {
+    color: '#fff',
+    background: 'hsl(351, 59%, 69%)',
+    ':hover': {
+      background: 'hsl(351, 59%, 60%)',
+    }
+  }
+};
+
 const Button = (props: PropsWithChildren<ButtonProps>) => {
+
+  const {
+    theme = 'primary',
+    onClick = false,
+  } = props;
+
+  const buttonProps: any = {};
+
+  if (onClick !== false) {
+    buttonProps.onClick = onClick;
+  }
+
+  const themeStyles = themes[theme];
+
   return (
     <button
-      { ...rule }
-      onClick={props.onClick}
+      { ...css(rules, themeStyles) }
+      { ...buttonProps }
     >
       { props.children }
     </button>
