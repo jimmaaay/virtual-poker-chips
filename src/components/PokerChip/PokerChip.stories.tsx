53e9073b41761e0eea6c5cb715e25e51df25faf1
jimmaaay <thisguy@jimmythompson.me>
2019-09-26 12:26:32 +0100

    WIP PokerChip component

--- src/components/PokerChip/PokerChip.stories.tsx
+++ src/components/PokerChip/PokerChip.stories.tsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import { action } from '@storybook/addon-actions';
-import PokerChip from './PokerChip';
+import PokerChip, { chipColours } from './PokerChip';
 
 export default {
   title: 'PokerChip',
@@ -9,11 +9,41 @@ export default {
 
 export const colours = () => (
   <Fragment>
+    { Object.keys(chipColours).map((chipValue) => {
+      return (
+        <PokerChip
+          chipValue={parseInt(chipValue) as any}
+          onClick={action('clicked')}
+        />
+      );
+    }) }
+  </Fragment>
+);
+
+export const transparent = () => (
+  <Fragment>
     <PokerChip
-      chipValue={1}
+      chipValue={500}
       onClick={action('clicked')}
-    >
-      100
-    </PokerChip>
+    />
+    <PokerChip
+      chipValue={500}
+      onClick={action('clicked')}
+      transparent
+    />
+  </Fragment>
+);
+
+export const disabled = () => (
+  <Fragment>
+    <PokerChip
+      chipValue={500}
+      onClick={action('clicked')}
+    />
+    <PokerChip
+      chipValue={500}
+      onClick={action('clicked')}
+      disabled
+    />
   </Fragment>
 );
